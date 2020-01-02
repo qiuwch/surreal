6819e87ec77cb04e61cfef2936697c8717af4053
Weichao Qiu <qiuwch@gmail.com>
2020-01-01 23:38:44 -0500

    Minor update.

--- render.sh
+++ render.sh
@@ -2,6 +2,6 @@
 # "$BLENDER"
 # alias blender='/mnt/c/Program\ Files/Blender\ Foundation/Blender/blender.exe'
 # blender
-blender="$HOME/blender-2.79b-linux-glibc219-x86_64/blender"
+blender="../blender-2.79b-linux-glibc219-x86_64/blender"
 cd datageneration
 $blender -b -t 1 -P main_part1.py -- --idx 0 --ishape 0 --stride 50
